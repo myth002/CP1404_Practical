5e70b28add1737cc213bee5eb3598e416d814f85
myth002 <anumolusandeep.anumolusandeepprasad@my.jcu.edu.au>
2016-08-01 14:25:05 +0800

    Testing

--- getNumber.py
+++ getNumber.py
@@ -1,15 +1,23 @@
 import string
 
 def main():
-    flag == False
+    flag = False
     while flag == False:
         flag=get_number(10,50)
 
 def get_number(lower,upper):
-    a=int(input("Enter a number({:d}-{:d})").format(lower,upper))
-    if (a<lower) or (a>upper):
+    a=int(input("Enter a number({:d} - {:d})\n>>>".format(lower,upper)))
+    try:
+        for x in range(0,len(a)):
+            y = a[x]
+            if y in string.digits:
+                continue
+    except:
+        print("Please enter a valid number")
+        return False
+    if a<lower or a>upper:
         print("Please enter a valid number!")
         return False
     else:
-        return True
+         return True
 main()
